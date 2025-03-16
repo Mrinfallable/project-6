d16faa3425cc5338383fbc8dc1e80a4e2a9fc590
Mrinfallable <131833559+Mrinfallable@users.noreply.github.com>
2025-03-16 15:41:21 -0400

    Create CItyinaustralia.java

---
+++ CItyinaustralia.java
@@ -0,0 +1,13 @@
+class CityInAustralia {
+	
+    private String name;
+    private int population;
+    private String[] hospitals;
+
+    public CityInAustralia(String n, int p, String[] h){
+        name = n;
+        population = p;
+        hospitals = h;
+    }
+
+}
